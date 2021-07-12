1f04e1d12c5927f8b1c909c768e4a970fdb03c12
grant-create <grant.s.wilkie@gmail.com>
2021-07-12 01:10:09 -0700

    axios on welcome

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -51,7 +51,6 @@ export default function Register(props) {
         }
         
         
-        
         console.log('submit the form')
     }
 
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,23 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+
+
+
 export default function Welcome() {
 
 
 
+    const [parkData, setParkData] = useState([])
+    let API_KEY = process.env.API_KEY
+    useEffect(() => {
+        axios.get(`https://developer.nps.gov/api/v1/parks?parkCode=acad&${API_KEY}`)
+        .then((response) => {
+          setParkData(response)
+          console.log("log",response)
+        })
+        .catch((err) => console.log(err))
+      }, [])
 
 
 
