3c12c63799d793fbb27f5042c87ced8adbe66229
Brian Young <berkshirehunt@gmail.com>
2021-07-16 13:56:46 -0700

    Navbar rendering

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,15 +11,11 @@ const API_KEY = process.env.REACT_APP_WEATHER_API_KEY
 export default function Navigation(props) {
     
   // // weather function, include city, weather state, icon, degrees F
-  const [weather, setWeather] = useState({})
+  const [weather, setWeather] = useState(null)
+
   const city = ['sitka', 'kalaupapa', 'jensen', 'patchogue']
 
   useEffect(() => {
-    // const api ={
-    //   key: "",
-    //   base: "https://api.openweathermap.org/data/2.5"
-    // }
-
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city[1]}&units=imperial&APPID=${API_KEY}`)
     .then(res => res.json())
     .then(result => {
@@ -32,25 +28,22 @@ export default function Navigation(props) {
     })
   }, [])
 
+
 const Weather = () => {
   return(
 
-  <div className="d-flex text-right justify-content-end text-white w-100 p-3">
-  <div className="text-right mr-2" style={{fontSize: '.9rem'}}>
-  {weather.name} weather:  
+    <div className="d-flex text-right justify-content-end w-100 p-3">
+      <div className="text-right mr-2" style={{fontSize: '.9rem', color: '#ccc'}}>
+        {weather.name} weather: &nbsp;
+        {weather.weather[0].description}  
+        <WiDaySunny size={24} color='#ccc' /> 
+        {Math.round(weather.main.temp)}&deg;F &nbsp;
+        <WiStrongWind size={24} color='#ccc' />
+        {weather.wind.speed}mph
+      </div>
+    </div>
 
-  {/* if (weather.weather != undefined){ */}
-   {/* {weather.weather[0].description}  */}
-  {/* } */}
-  {/* <WiDaySunny size={24} color='#fff' />  */}
-  {/* {Math.round(weather.main.temp)}&deg;F  */}
-  {/* <WiStrongWind size={24} color='#fff' />  */}
-  {/* {weather.wind.speed}mph */}
-  </div>
-</div>
-
-
-)
+  )
 }
 
     // ---------------------------- if the user is logged IN
@@ -61,11 +54,7 @@ const Weather = () => {
           <Nav.Link href="/" style={{borderRight: '3px solid #454e56', paddingRight: '30px'}}><img src={logoMain} /></Nav.Link>
           <Nav.Link style={{marginTop: '20px', paddingLeft: '30px'}} href="/profile"><FaHeart/></Nav.Link><div style={{marginTop: '28px', paddingLeft: '20px', color: '#454e54'}}>|</div><Nav.Link style={{marginTop: '20px', paddingLeft: '30px'}} href="/"><span onClick={props.handleLogout}>Logout</span></Nav.Link>
         </Nav>
-
-
-<Weather />
-
-        
+        {weather ? <Weather/> : ''}
       </Navbar>
     </>
     )
@@ -78,9 +67,7 @@ const Weather = () => {
           <Nav.Link style={{marginTop: '20px', paddingLeft: '30px'}} href="/login">Log in</Nav.Link><span style={{marginTop: '28px', paddingLeft: '10px', color: '#454e54'}}>|</span><Nav.Link style={{marginTop: '20px', paddingLeft: '20px'}} href="/register">Register</Nav.Link>
         </Nav>
 
-
-        <Weather />
-
+        {weather ? <Weather/> : ''}
 
       </Navbar>
     </>
